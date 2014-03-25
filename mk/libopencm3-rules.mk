a274ff87bc0eeccc1ee24097f7826e5ba55d8803
gr4viton <lordmutty@gmail.com>
2014-03-25 14:39:25 +0100

    deleted all

--- mk/libopencm3-rules.mk
+++
@@ -1,3 +0,0 @@
-$(OPENCM3_LIBDEP):
-	@$(PRINTF) "  MAKE    $@"
-	$(Q)$(MAKE) -C $(OPENCM3_DIR)
--- src/led_f4.c
+++
@@ -1,8 +0,0 @@
-#include "led_f4.h"
-
-void INIT_leds(void)
-{
-	rcc_periph_clock_enable(RCC_PLED);
-	gpio_mode_setup(PLED, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LED_ALL);
-	gpio_clear(PLED, LED_ALL);
-}
